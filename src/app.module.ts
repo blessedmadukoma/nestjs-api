a0295e5108e249ec146a92397518253c720108f9
blessedmadukoma <blessedmadukoma@gmail.com>
2024-02-01 23:17:00 +0100

    API essentials: set global prefix, CORS, rate limiting, throttle, logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,40 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
 import { EmployeesModule } from './employees/employees.module';
+import { UsersModule } from './users/users.module';
+import { MyLoggerModule } from './my-logger/my-logger.module';
 
 @Module({
-  imports: [UsersModule, DatabaseModule, EmployeesModule],
+  imports: [
+    UsersModule,
+    DatabaseModule,
+    EmployeesModule,
+    ThrottlerModule.forRoot([
+      {
+        name: 'short',
+        // ttl: 60000, // 1 minute
+        ttl: 1000, // 1 minute
+        limit: 3, // 3 requests per 1 minute
+      },
+      {
+        name: 'long',
+        ttl: 3600, // 1 hour
+        limit: 100, // 100 requests per hour
+      },
+    ]),
+    MyLoggerModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
+// import { MyLoggerService } from 'src/my-logger/my-logger.service';
+import { AllExceptionsFilter } from 'src/all-exceptions.filter';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    // bufferLogs: true,
+  });
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
+
+  // app.useLogger(app.get(MyLoggerService));
+  app.enableCors();
+  app.setGlobalPrefix('api');
+
   await app.listen(3000);
 }
 bootstrap();
